173eef3f51a3c6c927866a77a7626cba80366d67
nikopolo <bolliboo@yandex.ru>
2026-04-29 21:14:04 +0200

    KS-3-3: fix to output from a new line

--- src/Main.kt
+++ src/Main.kt
@@ -1,5 +1,5 @@
 
 fun main() {
     val numb = 5
-    println("$numb x 1 = ${numb * 1} $numb x 2 = ${numb * 2} $numb x 3 = ${numb * 3} $numb x 4 = ${numb*4} $numb x 5 = ${numb*5} $numb x 6 = ${numb*6} $numb x 7 = ${numb*7} $numb x 8 = ${numb*8} $numb x 9 = ${numb*9}")
+    println("$numb x 1 = ${numb * 1}\n$numb x 2 = ${numb * 2}\n$numb x 3 = ${numb * 3}\n$numb x 4 = ${numb * 4}\n$numb x 5 = ${numb * 5}\n$numb x 6 = ${numb * 6}\n$numb x 7 = ${numb * 7}\n$numb x 8 = ${numb *8 }\n$numb x 9 = ${numb * 9}")
 }
