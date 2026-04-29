f65e0afcf4915eca679b7911b3d1f5774d1ce9c6
nikopolo <bolliboo@yandex.ru>
2026-04-29 22:55:12 +0200

    KS-3-4: move in chess

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,13 @@
 
 fun main() {
-    println("hello")
+    var from = "E2"
+    var where = "E4"
+    var count = 1
+    var lineToSend = "$from-$where;$count"
+    println(lineToSend)
+    from = "D2"
+    where = "D3"
+    count++
+    lineToSend = "$from-$where;$count"
+    println(lineToSend)
 }
