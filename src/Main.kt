116d2526acba2223508cfac946b46e4f42852304
nikopolo <bolliboo@yandex.ru>
2026-04-29 21:08:20 +0200

    KS-3-3: multiplication table

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 
 fun main() {
-    println("hello")
+    val numb = 5
+    println("$numb x 1 = ${numb * 1} $numb x 2 = ${numb * 2} $numb x 3 = ${numb * 3} $numb x 4 = ${numb*4} $numb x 5 = ${numb*5} $numb x 6 = ${numb*6} $numb x 7 = ${numb*7} $numb x 8 = ${numb*8} $numb x 9 = ${numb*9}")
 }
