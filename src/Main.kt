9468471f3033ac9eb1fb2f00498c6c1cdb4446aa
nikopolo <bolliboo@yandex.ru>
2026-04-29 23:05:54 +0200

    KS-3-5: server parser

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,11 @@
 
 fun main() {
-    println("hello")
+    val userTurn = "D2-D4;0"
+    val inputParse = userTurn.split("-", ";")
+    val from = inputParse[0]
+    println(from)
+    val where = inputParse[1]
+    println(where)
+    val count = inputParse[2]
+    println(count)
 }
