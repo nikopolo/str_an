bc291e9153f18f45a1226d850a445b5cf24d9e6d
nikopolo <bolliboo@yandex.ru>
2026-04-29 19:15:13 +0200

    KS-3-1: string greeting

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,8 @@
 
 fun main() {
-    println("hello")
+    val name = "User"
+    var greetings = "Good day, $name!"
+    println(greetings)
+    greetings = "Good evening, $name!"
+    println(greetings)
 }
